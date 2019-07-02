b4a3fc98646761b46c5923fe54134679fdc1d652
pmj_glb <2654771064_glb@qq.com>
2019-07-02 22:04:36 +0800

    ooooo

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -3,8 +3,7 @@ package com.hand.git;
 public class Happy {
 
 	public static void main(String[] args) {
-		System.out.println("test22.....");
-		System.out.println("test11.....");
+		System.out.println("test.....");
 
 	}
 
