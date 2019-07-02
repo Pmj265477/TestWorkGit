fb5e840b3880597ef5666880433cbdabeaf23fa4
pmj_glb <2654771064_glb@qq.com>
2019-07-02 18:22:14 +0800

    Work first commit 1

--- src/main/java/com/hand/git/Apple.java
+++ src/main/java/com/hand/git/Apple.java
@@ -3,6 +3,6 @@ package com.hand.git;
 public class Apple {
 
 public static void main(String[] args) {
-	System.out.println("A1...");
+	System.out.println("A...");
 }
 }
--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -3,7 +3,7 @@ package com.hand.git;
 public class Happy {
 
 	public static void main(String[] args) {
-		System.out.println("test1.....");
+		System.out.println("test.....");
 
 	}
 
